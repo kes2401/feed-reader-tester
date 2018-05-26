fa67ebb788554b058b81b327f60a86fc9dadb385
kes2401 <keithscully83@gmail.com>
2018-05-26 14:11:47 +0100

    Initial Entries suite complete

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -85,7 +85,16 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        beforeEach(function(done) {
+            loadFeed(0, function(){
+                done();
+            })
+        });
 
+        it('loadFeed runs and has at least one entry', function(done) {
+            expect($('.feed').children().length).not.toBe(0);
+            done();
+        });
     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
